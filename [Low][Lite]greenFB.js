98bcc487c624486bd15b96d8c16f60e319374017
LAPTOP-AVQQN1Q5\NawaNawa <t105590029@ntut.org.tw>
2017-07-28 21:56:03 +0800

    Lite中Style改用function

--- [Low][Lite]greenFB.js
+++ [Low][Lite]greenFB.js
@@ -11,14 +11,18 @@ function setIcon(icon) {
         }
         ) 
 }
+function createStyle()
+{
+    var newStyle = document.createElement("style");
+    newStyle.innerHTML += "* {background-color: rgb(100, 200, 100);}";
+    newStyle.innerHTML += ".UFIRow {background-color: rgb(100, 200, 100);}";
+    newStyle.innerHTML += "div {background-color: rgb(100, 200, 100);}";
+    newStyle.innerHTML += "a {color:rgba(0,50,0,100);background-color: rgba(0,0,0,0);}";
+    newStyle.innerHTML += "span{background-color:rgba(0,0,0,0);background-color: rgba(0,0,0,0)}";
+    newStyle.innerHTML += "p{background-color:rgba(0,0,0,0)}";
+    newStyle.innerHTML += "li{background-color:rgba(100,200,100,100)}";
+    newStyle.innerHTML += "input{background-color:rgba(100,200,100,100)}";
+    document.querySelector('head').appendChild(newStyle);
+}
 setIcon('http://imgur.com/d6kJJp8.png');//改FB的icon成綠色的
-var newStyle = document.createElement("style");
-newStyle.innerHTML += "* {background-color: rgb(100, 200, 100);}";
-newStyle.innerHTML += ".UFIRow {background-color: rgb(100, 200, 100);}";
-newStyle.innerHTML += "div {background-color: rgb(100, 200, 100);}";
-newStyle.innerHTML += "a {color:rgba(0,50,0,100);background-color: rgba(0,0,0,0);}";
-newStyle.innerHTML += "span{background-color:rgba(0,0,0,0);background-color: rgba(0,0,0,0)}";
-newStyle.innerHTML += "p{background-color:rgba(0,0,0,0)}";
-newStyle.innerHTML += "li{background-color:rgba(100,200,100,100)}";
-newStyle.innerHTML += "input{background-color:rgba(100,200,100,100)}";
-document.querySelector('head').appendChild(newStyle);
+createStyle();
